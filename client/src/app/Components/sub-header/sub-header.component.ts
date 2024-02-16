119003b154202960dffd1b576e4758170d649d75
Bob-development <proinvoke228@gmail.com>
2024-02-16 04:24:33 +0200

    header/footer done

---
+++ client/src/app/Components/sub-header/sub-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-header',
+  templateUrl: './sub-header.component.html',
+  styleUrl: './sub-header.component.scss'
+})
+export class SubHeaderComponent {
+
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+//COMPONENTS
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SubHeaderComponent } from './Components/sub-header/sub-header.component';
+
+//MODULES
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
+  declarations: [
+    AppComponent, 
+    HeaderComponent, 
+    FooterComponent, 
+    SubHeaderComponent
+  ],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule
+  ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
