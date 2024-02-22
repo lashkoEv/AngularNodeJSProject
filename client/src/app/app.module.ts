374b9f97ebd3e968c5b7431173ebd4692b231387
Bob-development <proinvoke228@gmail.com>
2024-02-23 00:59:18 +0200

    asd

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,8 +15,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
-import { AlertModule } from '@coreui/angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,9 +25,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule, 
-    BrowserAnimationsModule,
     AppRoutingModule, 
-    AlertModule,
     MatButtonModule,
     MatIconModule,
     MatInputModule,
