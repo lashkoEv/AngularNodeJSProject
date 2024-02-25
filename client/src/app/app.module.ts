96d8c8127066d783ae57e43a5a4302cb75709fbb
Bob-development <proinvoke228@gmail.com>
2024-02-25 22:44:00 +0200

    add coreUi

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 
 //COMPONENTS
@@ -11,6 +13,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { CarouselModule } from '@coreui/angular';
+import { IconModule, IconSetService } from '@coreui/icons-angular';
 
 @NgModule({
   declarations: [
@@ -19,13 +23,17 @@ import { MatMenuModule } from '@angular/material/menu';
   ],
   imports: [
     BrowserModule, 
+    BrowserAnimationsModule,
     AppRoutingModule, 
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatMenuModule
+    MatMenuModule,
+    CarouselModule,
+    IconModule
   ],
-  providers: [],
+  providers: [IconSetService, provideAnimationsAsync()],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/carousel/carousel.component.ts
+++ client/src/app/carousel/carousel.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './carousel.component.scss'
 })
 export class CarouselComponent {
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      id: 0,
+      src: './assets/32665E6F-8D90-4567-9769D59E11DB7F26_source.webp',
+      title: 'First slide',
+      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+    };
+    this.slides[1] = {
+      id: 1,
+      src: './assets/cat_thumb_1681303010348_1681303048932_1681303048932.avif',
+      title: 'Second slide',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    }
+    this.slides[2] = {
+      id: 2,
+      src: './assets/neva-masquerade-royalty-free-image-1674509896.jpg',
+      title: 'Third slide',
+      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+    }
+  }
 }
