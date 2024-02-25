e363ac7a9179c14f5dd32a3ef504dfb8bf219bdf
Bob-development <proinvoke228@gmail.com>
2024-02-25 22:20:14 +0200

    Pull smth

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 //COMPONENTS
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { SubHeaderComponent } from './Components/sub-header/sub-header.component';
 import { CarouselComponent } from './carousel/carousel.component';
 
 //MODULES
@@ -19,9 +15,7 @@ import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
   declarations: [
     AppComponent, 
-    HeaderComponent, 
-    FooterComponent, 
-    SubHeaderComponent, CarouselComponent
+    CarouselComponent
   ],
   imports: [
     BrowserModule, 
@@ -31,7 +25,7 @@ import { MatMenuModule } from '@angular/material/menu';
     MatInputModule,
     MatMenuModule
   ],
-  providers: [provideAnimationsAsync()],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
