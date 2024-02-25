89cf2427a67637f8c8de7ec545d07287e40ee669
XpAHiTeJl <robengud2@gmail.com>
2024-02-25 22:53:12 +0200

    merge

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
