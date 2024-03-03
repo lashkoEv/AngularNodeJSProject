f72b971855366864ebfaa1a6800d9d5949c837a3
Bob-development <proinvoke228@gmail.com>
2024-03-03 21:17:11 +0200

    carousel done

--- client/src/app/carousel/carousel.component.ts
+++ client/src/app/carousel/carousel.component.ts
@@ -13,19 +13,19 @@ export class CarouselComponent {
   ngOnInit(): void {
     this.slides[0] = {
       id: 0,
-      src: './assets/32665E6F-8D90-4567-9769D59E11DB7F26_source.webp',
+      src: './../../assets/carouselImg/img1.jpg',
       title: 'First slide',
       subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
     };
     this.slides[1] = {
       id: 1,
-      src: './assets/cat_thumb_1681303010348_1681303048932_1681303048932.avif',
+      src: './../../assets/carouselImg/img2.jpg',
       title: 'Second slide',
       subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     }
     this.slides[2] = {
       id: 2,
-      src: './assets/neva-masquerade-royalty-free-image-1674509896.jpg',
+      src: './../../assets/carouselImg/img3.jpg',
       title: 'Third slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
     }
