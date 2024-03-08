228ebbd7682317ca2209a56ea76bc39ebc909a15
michaelzarubin24 <129330244+michaelzarubin24@users.noreply.github.com>
2024-03-08 12:22:26 +0200

    header styles added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
