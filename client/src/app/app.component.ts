26d8af968e0f4249460cffce359b9c376999cbd8
Евгения Лашко <smevlk2014@gmail.com>
2024-03-12 23:29:41 +0200

    merge from Yaroslav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,76 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from './services/product.service';
-import { CategoryService } from './services/category.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { SpinnerService } from './services/spinner.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  constructor(
-    private productService: ProductService,
-    private categoryService: CategoryService
-  ) {
-    // ! Products
-    // productService.getAll().forEach((data) => {
-    //   console.log(data);
-    // });
-    // productService
-    //   .getById('65edf336bc94ee73edc6c7eb')
-    //   .subscribe((data) => console.log(data));
-    // productService
-    //   .add({
-    //     title: 'Title',
-    //     description: 'Description',
-    //     country: 'Country',
-    //     count: 10,
-    //     price: 100.55,
-    //     fields: 'fields',
-    //     category: 'Category',
-    //     imgSrc: 'src',
-    //   })
-    //   .subscribe((data) => console.log(data));
-    // productService
-    //   .deleteById('65edf336bc94ee73edc6c7eb')
-    //   .subscribe((data) => console.log(data));
-    // productService
-    //   .update({
-    //     _id: '65edfe20c4bb05a2dfdce4eb',
-    //     title: 'Title2',
-    //     description: '222Description',
-    //     country: '22222Country',
-    //     count: 10,
-    //     price: 100.55,
-    //     fields: '222fields',
-    //     category: '2Category',
-    //     imgSrc: '2222src',
-    //   })
-    //   .subscribe((data) => console.log(data));
-    // ! Categories
-    // categoryService
-    //   .add({
-    //     title: 'Title',
-    //     description: 'Description',
-    //     imgSrc: 'src',
-    //   })
-    //   .subscribe((data) => console.log(data));
-    // categoryService.getAll().forEach((data) => {
-    //   console.log(data);
-    // });
-    // categoryService
-    //   .getById('65ee08b7a0b910db62048747')
-    //   .subscribe((data) => console.log(data));
-    // categoryService
-    //   .deleteById('65ee08bca0b910db6204874a')
-    //   .subscribe((data) => console.log(data));
-    // categoryService
-    //   .update({
-    //     _id: '65ee08b7a0b910db62048747',
-    //     title: 'Title2',
-    //     description: '222Description',
-    //     imgSrc: '2222src',
-    //   })
-    //   .subscribe((data) => console.log(data));
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(private router: Router, private spinner: SpinnerService) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.spinner.show();
+        setTimeout(() => {
+          this.spinner.hide();
+        }, 3000);
+      }
+    });
   }
+
+  ngOnDestroy(): void {}
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,9 +11,21 @@ import { HeaderComponent } from './components/header/header.component';
 import { ConsultationComponent } from './components/consultation/consultation.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CalloutModule } from '@coreui/angular';
+
+import { AppComponent } from './app.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { IconModule, IconSetService } from '@coreui/icons-angular';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ConsultationComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ConsultationComponent,
+    SpinnerComponent,
+    NotificationComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -22,8 +34,12 @@ import { HttpClientModule } from '@angular/common/http';
     MatInputModule,
     MatIconModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    CalloutModule,
+    IconModule,
   ],
-  providers: [provideAnimationsAsync()],
+  providers: [provideAnimationsAsync(), IconSetService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
