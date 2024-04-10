77b5601140dceacfece1f8acc98dba85d5ea7090
l1t4n <maxivan200206@gmail.com>
2024-04-10 23:38:02 +0200

    products-carousel component changed: now render and routes to the real catalog

--- client/src/app/components/products-carousel/products-carousel.component.ts
+++ client/src/app/components/products-carousel/products-carousel.component.ts
@@ -21,79 +21,79 @@ export class ProductsCarouselComponent {
   }
   /////////////////////////////////////////////////////////////////////////
 
-  public slides: any[] = [
-    {
-      id: 0,
-      src: './../../assets/productsImg/abestosProductsATI.webp',
-      title: 'АЗБЕСТОВІ ВИРОБИ АТІ',
-      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
-      link: ''
-    },
+  // public slides: any[] = [
+  //   {
+  //     id: 0,
+  //     src: './../../assets/productsImg/abestosProductsATI.webp',
+  //     title: 'АЗБЕСТОВІ ВИРОБИ АТІ',
+  //     subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+  //     link: ''
+  //   },
 
-    {
-      id: 1,
-      src: './../../assets/productsImg/asbestosClothAT.webp',
-      title: 'ТКАНИНА АЗБЕСТОВА АТ (АСБОТКАНЬ)',
-      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      link: ''
-    },
-    {
-      id: 2,
-      src: './../../assets/productsImg/asbestosCordShaun.webp',
-      title: 'АЗБЕСТОВИЙ ШНУР ШАОН ГОСТ 1779-83',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    },
-    {
-      id: 3,
-      src: './../../assets/productsImg/asbestosElectricalInsulatingTapeLAE.webp',
-      title: 'СТРІЧКА АЗБЕСТОВА ЕЛЕКТРОІЗОЛЯЦІЙНА ЛАЭ',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    },
-    {
-      id: 4,
-      src: './../../assets/productsImg/brakeBeltLat2.webp',
-      title: 'СТРІЧКА ГАЛЬМІВНА ЛАТ-2, ЕМ-1, ЕМ-2(К)',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    },
-    {
-      id: 5,
-      src: './../../assets/productsImg/fireResistantFabrics.webp',
-      title: 'ВОГНЕТРИВКІ ТКАНИНИ',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    },
-    {
-      id: 6,
-      src: './../../assets/productsImg/paronitPonB.webp',
-      title: 'ПАРОНІТ ПОН-Б, ПМБ',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    },
-    {
-      id: 7,
-      src: './../../assets/productsImg/purchaseSalnikov.webp',
-      title: 'НАБИВКА САЛЬНИКОВА',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    },
-    {
-      id: 8,
-      src: './../../assets/productsImg/rawRubber.webp',
-      title: 'СИРА ГУМА (ГУМОВА СУМІШ)',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    },
-    {
-      id: 9,
-      src: './../../assets/productsImg/rubberEngineeringProductsGTV.webp',
-      title: 'РЕЗИНОТЕХНИЧНЫЕ ВИРОБИ ГТВ',
-      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
-      link: ''
-    }
-  ]
+  //   {
+  //     id: 1,
+  //     src: './../../assets/productsImg/asbestosClothAT.webp',
+  //     title: 'ТКАНИНА АЗБЕСТОВА АТ (АСБОТКАНЬ)',
+  //     subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 2,
+  //     src: './../../assets/productsImg/asbestosCordShaun.webp',
+  //     title: 'АЗБЕСТОВИЙ ШНУР ШАОН ГОСТ 1779-83',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 3,
+  //     src: './../../assets/productsImg/asbestosElectricalInsulatingTapeLAE.webp',
+  //     title: 'СТРІЧКА АЗБЕСТОВА ЕЛЕКТРОІЗОЛЯЦІЙНА ЛАЭ',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 4,
+  //     src: './../../assets/productsImg/brakeBeltLat2.webp',
+  //     title: 'СТРІЧКА ГАЛЬМІВНА ЛАТ-2, ЕМ-1, ЕМ-2(К)',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 5,
+  //     src: './../../assets/productsImg/fireResistantFabrics.webp',
+  //     title: 'ВОГНЕТРИВКІ ТКАНИНИ',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 6,
+  //     src: './../../assets/productsImg/paronitPonB.webp',
+  //     title: 'ПАРОНІТ ПОН-Б, ПМБ',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 7,
+  //     src: './../../assets/productsImg/purchaseSalnikov.webp',
+  //     title: 'НАБИВКА САЛЬНИКОВА',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 8,
+  //     src: './../../assets/productsImg/rawRubber.webp',
+  //     title: 'СИРА ГУМА (ГУМОВА СУМІШ)',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   },
+  //   {
+  //     id: 9,
+  //     src: './../../assets/productsImg/rubberEngineeringProductsGTV.webp',
+  //     title: 'РЕЗИНОТЕХНИЧНЫЕ ВИРОБИ ГТВ',
+  //     subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.',
+  //     link: ''
+  //   }
+  // ]
 
   public slidesID: number[] = [0, 1, 2, 3, 4];
 
