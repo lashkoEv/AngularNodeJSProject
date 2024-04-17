ca4c29eb3ca5d39e97812d648a6c722fa31c4554
Bob-development <proinvoke228@gmail.com>
2024-04-17 14:25:30 +0300

    fix product carousel

--- client/src/app/components/products-carousel/products-carousel.component.ts
+++ client/src/app/components/products-carousel/products-carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 
 @Component({
@@ -6,12 +6,32 @@ import { CategoryService } from '../../services/category.service';
   templateUrl: './products-carousel.component.html',
   styleUrl: './products-carousel.component.scss'
 })
-export class ProductsCarouselComponent {
+export class ProductsCarouselComponent implements OnInit{
 
   /////////////////////////////////////////////////////////////////////////
-  public categories: any;
+  public responsiveOptions: any[] | undefined;
+  public categories: any[];
+
   ngOnInit(): void {
-    this.getCategories();
+    this.getCategories();    
+
+    this.responsiveOptions = [
+      {
+          breakpoint: '1199px',
+          numVisible: 1,
+          numScroll: 1
+      },
+      {
+          breakpoint: '991px',
+          numVisible: 2,
+          numScroll: 1
+      },
+      {
+          breakpoint: '767px',
+          numVisible: 1,
+          numScroll: 1
+      }
+  ];
   }
 
   async getCategories() {
@@ -97,7 +117,7 @@ export class ProductsCarouselComponent {
 
   public slidesID: number[] = [0, 1, 2, 3, 4];
 
-  constructor(private categoryService: CategoryService) {} ///////////////////////////////////////////
+  constructor(private categoryService: CategoryService) {}
 
   public increaseId() {
     this.slidesID = this.slidesID.map(id => (id + 1) % this.categories.length);
@@ -106,4 +126,5 @@ export class ProductsCarouselComponent {
   public reduceId() {
     this.slidesID = this.slidesID.map(id => (id - 1 + this.categories.length) % this.categories.length);
   }
+
 }
