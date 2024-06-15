bbb6b5dc49d0aa2ec42e04b92202f9eb9db3da47
michaelzarubin24 <129330244+michaelzarubin24@users.noreply.github.com>
2024-06-15 22:26:49 +0300

    notifications

--- client/src/app/components/consultation/consultation.component.ts
+++ client/src/app/components/consultation/consultation.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ConsultationService } from '../../services/consultation.service';
+import { MessageService } from 'primeng/api';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-consultation',
@@ -7,18 +9,37 @@ import { ConsultationService } from '../../services/consultation.service';
   styleUrl: './consultation.component.scss',
 })
 export class ConsultationComponent {
+  @ViewChild('form') form!: NgForm;
   public inputEmail: string = '';
   public inputTopic: string = '';
   public inputMessage: string = '';
 
-  constructor(private consultationService: ConsultationService) {}
+  constructor(
+    private consultationService: ConsultationService,
+    private messageService: MessageService
+  ) {}
 
   public add(data: any) {
     console.log(data);
-    this.consultationService.add(data).subscribe((data) => {
-      if (data.ok) {
-        console.log(data);
+    this.consultationService.add(data).subscribe(
+      (response) => {
+        if (response.ok) {
+          console.log(response);
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Сообщение отправлено!',
+            detail: 'Сообщение отправлено!',
+          });
+          this.form.reset();
+        }
+      },
+      (error) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Ошибка',
+          detail: 'Не удалось отправить сообщение.',
+        });
       }
-    });
+    );
   }
 }
