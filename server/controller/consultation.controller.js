17fd57e8c33096be019cd9969c9ac847cd6d82dc
michaelzarubin24 <129330244+michaelzarubin24@users.noreply.github.com>
2024-11-12 00:04:02 +0200

    1

--- server/controller/consultation.controller.js
+++ server/controller/consultation.controller.js
@@ -22,7 +22,7 @@ async function add(req, res) {
 
     const adminOptions = {
       from: "zarubinmihail99@gmail.com",
-      to: "ipstorg@gmail.com",
+      to: "zarubinmihail99@gmail.com",
       subject: "Новий запит на консультацію",
       text: `Новий запит на консультацію від ${consultationData.email}. Тема: ${consultationData.topic}. Повідомлення: ${consultationData.message}`,
     };
